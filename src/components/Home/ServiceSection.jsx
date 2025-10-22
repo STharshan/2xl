9b9ce08dff047c4ed34e535de865299fa1b8f7eb
STharshan <sivatharshan81@gmail.com>
2025-10-22 14:08:44 +0530

    a

--- src/components/Home/ServiceSection.jsx
+++ src/components/Home/ServiceSection.jsx
@@ -6,21 +6,21 @@ import "aos/dist/aos.css";
 
 const services = [
   {
-    title: "Alloy Wheel Repair",
-    desc: `Remove scuffs, scratches, and damage for wheels that look brand new.`,
+    title: "Mot",
+    desc: `Fast, hassle-free MOT testing to keep you road-legal and safe.`,
     image: "/carpaint.avif",
     path: "/services/mechanical",
   },
   {
-    title: "Dent Removal",
-    desc: `Seamless dent repair to restore panels back to factory condition.
+    title: "Service",
+    desc: `Full car servicing to keep your vehicle running smoothly and efficiently.
 ​`,
     image: "/gray.avif",
     path: "/services/service",
   },
   {
-    title: "Bumper Repair",
-    desc: `Repair cracked or scuffed bumpers to a smooth, like-new finish.`,
+    title: "Mechanical",
+    desc: `The repair and maintenance of a vehicle's engine, transmission, brakes, suspension, and other vital components`,
     image: "/carmechanic.avif",
     path: "/services/mot",
   },
@@ -149,7 +149,7 @@ const ServiceSection = () => {
                   <img
                     src={service.image}
                     alt={service.title}
-                    className="absolute inset-0 w-[85%] mt-15 mx-auto h-68 object-cover"
+                    className="absolute inset-0 w-[75%] mx-auto mt-15 h-65 object-cover"
                     onError={(e) => {
                       e.currentTarget.onerror = null;
                       e.currentTarget.src = "/logo.png"; // fallback logo
